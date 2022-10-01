6cf88c08417e1f996ca64eb245e0982eaee8c6e0
Guilherme Pressutto <gpressutto5@gmail.com>
2022-10-01 11:54:15 -0300

    Converting international purchases

--- convert.js
+++ convert.js
@@ -46,13 +46,41 @@ const maybeHandleInstalments = (row) => {
   return row;
 };
 var workbook = XLSX.readFile(inputPath);
-const data = XLSX.utils
-  .sheet_to_json(workbook.Sheets[workbook.SheetNames[0]], { header: 1 })
-  .filter(validateRow)
+const allData = XLSX.utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[0]], { header: 1 });
+const csvData = allData.filter(validateRow)
   .map(maybeHandleInstalments)
   .filter((row) => row !== null);
-data.unshift(["Date", "Payee", "", "Outflow"]);
+  csvData.unshift(["Date", "Payee", "", "Outflow"]);
 
-const csv = XLSX.utils.sheet_to_csv(XLSX.utils.aoa_to_sheet(data));
+// push international transactions
+const internationalStart = allData.findIndex((row) => row[0] === "lançamentos internacionais");
+const internationalEnd = allData.findIndex((row) => row[0] === "total de lançamentos internacionais (titular + adicionais)");
+if (internationalStart !== -1 && internationalEnd !== -1) {
+  const internationalSection = allData.slice(internationalStart + 1, internationalEnd);
+
+  const internationalTransactions = [];
+  for (let i = 0; i < internationalSection.length; i++) {
+    const row = internationalSection[i];
+    if (String(row[0]).match(/^\d{2}\/\d{2}\/\d{4}$/)) {
+      const date = row[0];
+      const payee = internationalSection[i+1][1];
+      const outflow = internationalSection[i+1][3];
+      internationalTransactions.push([date, payee, "", outflow]);
+    }
+  }
+
+  const iof = internationalSection.find((row) => row[0] === "IOF - transação internacional")[3];
+
+  // distribute iof among transactions by percentage
+  const totalInternationalOutflow = internationalTransactions.reduce((acc, row) => acc + row[3], 0);
+  internationalTransactions.forEach((row) => {
+    const percentage = row[3] / totalInternationalOutflow;
+    row[3] = Number((row[3] + (iof * percentage)).toFixed(2));
+  });
+
+  csvData.push(...internationalTransactions);
+}
+
+const csv = XLSX.utils.sheet_to_csv(XLSX.utils.aoa_to_sheet(csvData));
 console.log(`Writing ${outputPath}`);
 fs.writeFileSync(outputPath, csv);
--- readme.md
+++ readme.md
@@ -7,5 +7,3 @@ Exporte sua faura em xls e execute o comando:
 ```sh
 node convert.js --input {xls file path} [--output {csv file path}]
 ```
-
-Não suporta transações internacionais ainda.

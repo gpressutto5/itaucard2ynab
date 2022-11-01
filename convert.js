a066bdf9eb378caccb929d7b700c549df4479ef3
Guilherme Pressutto <gpressutto5@gmail.com>
2022-11-01 00:09:46 -0300

    Fixed IOF calculation

--- convert.js
+++ convert.js
@@ -46,29 +46,69 @@ const maybeHandleInstalments = (row) => {
   return row;
 };
 var workbook = XLSX.readFile(inputPath);
-const allData = XLSX.utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[0]], { header: 1 });
-const csvData = allData.filter(validateRow)
+const allData = XLSX.utils.sheet_to_json(
+  workbook.Sheets[workbook.SheetNames[0]],
+  { header: 1 }
+);
+const csvData = allData
+  .filter(validateRow)
   .map(maybeHandleInstalments)
   .filter((row) => row !== null);
-  csvData.unshift(["Date", "Payee", "", "Outflow"]);
+csvData.unshift(["Date", "Payee", "", "Outflow"]);
 
 // push international transactions
-const internationalStart = allData.findIndex((row) => row[0] === "lançamentos internacionais");
-const internationalEnd = allData.findIndex((row) => row[0] === "total de lançamentos internacionais (titular + adicionais)");
+const internationalStart = allData.findIndex(
+  (row) => row[0] === "lançamentos internacionais"
+);
+const internationalEnd = allData.findIndex(
+  (row) =>
+    row[0] === "total de lançamentos internacionais (titular + adicionais)"
+);
 if (internationalStart !== -1 && internationalEnd !== -1) {
-  const internationalSection = allData.slice(internationalStart + 1, internationalEnd);
+  const internationalSection = allData.slice(
+    internationalStart + 1,
+    internationalEnd
+  );
 
   const internationalTransactions = [];
-  for (let i = 0; i < internationalSection.length; i++) {
-    const row = internationalSection[i];
-    if (String(row[0]).match(/^\d{2}\/\d{2}\/\d{4}$/)) {
-      const date = row[0];
-      const payee = internationalSection[i+1][1];
-      const outflow = internationalSection[i+1][3];
-      const iof = Number(internationalSection.slice(i).find((row) => row[0] === "IOF - transação internacional")[3]);
-      internationalTransactions.push([date, payee, "", outflow + iof]);
+  let iofIndexes = internationalSection
+    .map((row, index) =>
+      row[0] === "IOF - transação internacional" ? Number(index) : null
+    )
+    .filter(Number);
+  const internationSectionParts = iofIndexes.map((iofIndex, index) =>
+    internationalSection.slice(
+      Number(iofIndexes[index - 1] || -1) + 1,
+      iofIndex + 1
+    )
+  );
+
+  internationSectionParts.forEach((part) => {
+    const partTransactions = [];
+    for (let i = 0; i < part.length; i++) {
+      const row = part[i];
+      if (String(row[0]).match(/^\d{2}\/\d{2}\/\d{4}$/)) {
+        const date = row[0];
+        const payee = part[i + 1][1];
+        const outflow = part[i + 1][3];
+        partTransactions.push([date, payee, "", outflow]);
+      }
     }
-  }
+    const iof = part.find(
+      (row) => row[0] === "IOF - transação internacional"
+    )[3];
+
+    // distribute iof among transactions by percentage
+    const totalInternationalOutflow = partTransactions.reduce(
+      (acc, row) => acc + row[3],
+      0
+    );
+    partTransactions.forEach((row) => {
+      const percentage = row[3] / totalInternationalOutflow;
+      row[3] = Number((row[3] + iof * percentage).toFixed(2));
+    });
+    internationalTransactions.push(...partTransactions);
+  });
 
   csvData.push(...internationalTransactions);
 }
